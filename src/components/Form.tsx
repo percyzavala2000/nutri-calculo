527540bb55b850c7c20ce8325b096f12462dab1f
percyzavala <percyzavala@gmail.com>
2024-06-10 22:59:11 -0500

    Se imlemento activity en  el useState en componente Form.tsx se creo el tipado de actividad

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,25 @@
+import { ChangeEvent, useState } from 'react';
 import { categoriesData } from "./data/categoriesData";
+import { ActivityT } from './types';
 
 export const Form = () => {
+  const [activity, setActivity] = useState<ActivityT>({
+    category:1,
+    name:"",
+    calories:0
+  })
+   //creo una funcion 
+  const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement> ) => { 
+
+    const isNumberField= ['category','calories'].includes(e.target.id);
+   
+
+    setActivity({...activity, [e.target.id]: isNumberField ? +e.target.value:e.target.value });
+
+    console.log(activity);
+
+
+   }
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
       <div className="grid grid-cols-2 gap-3 ">
@@ -10,7 +29,9 @@ export const Form = () => {
         <select
           name=""
           id="category"
+          value={activity.category}
           className="border border-slate-300 p-2 rounded-lg w-full bg-white "
+          onChange={handleChange}
         >
           {
             /* Aqui es lo que iteramos nuestro categoriesData */
@@ -23,26 +44,30 @@ export const Form = () => {
         </select>
       </div>
       <div className="grid grid-cols-2 gap-3">
-        <label htmlFor="activity" className="font-bold">
+        <label htmlFor="name" className="font-bold">
           Actividad:
         </label>
         <input
           type="text"
-          id="activity"
+          id="name"
           className=" border border-slate-300 p-2 rounded-lg"
           placeholder="Ejem: Comida, Jugo de papaya,ensaladas, ejercicio,pesas, bicicleta"
+          value={activity.name}
+          onChange={handleChange}
         />
       </div>
 
       <div className="grid grid-cols-2 gap-3">
         <label htmlFor="calorias" className="font-bold">
-          Actividad:
+          Calorias:
         </label>
         <input
           type="number"
-          id="calorias"
+          id="calories"
           className=" border border-slate-300 p-2 rounded-lg"
           placeholder="calorias: Ejemplo 300 o 500"
+          value={activity.calories}
+          onChange={handleChange}
         />
       </div>
       <input
--- src/components/types/index.ts
+++ src/components/types/index.ts
@@ -2,3 +2,8 @@ export type CategoryT = {
   id: number;
   name: string;
 };
+ export type ActivityT={
+  category:number,
+  name:string,
+  calories:number
+ }
